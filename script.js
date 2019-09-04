b029fe2048f8bc399bbd506a24d65aa15fc2797a
geo-liak <liakopoulos.georgios@gmail.com>
2019-09-04 09:20:20 +0300

    Pause feature. JSDoc. Border around board. EventListener.

--- script.js
+++ script.js
@@ -22,7 +22,9 @@
     let snake = [];
 
     /**
-     * @param {*} source Is an object which acts as a source of coordinates, like activeBlock
+     * @param {Object} source Is an object which acts as a source of coordinates, like activeBlock
+     * @param {number} source.row Is an object which acts as a source of coordinates, like activeBlock
+     * @param {number} source.column Is an object which acts as a source of coordinates, like activeBlock
      */
     function coordinates(source) {
         this.row = source.row;
@@ -50,13 +52,10 @@
     }
 
 
-    // functions
-    //
-    //
     /**
      * This function changes the state of a block by replacing its classes.
      * The classes are determined by the arguments "fromState" and "toState".
-     * @param {*} coordinates The position of the desired block to be changed is 
+     * @param {Object} coordinates The position of the desired block to be changed is 
      * determined by the argument "coordinates" which should take an object with 
      * the coordinates (row and column).
      * @param {string} fromState The class which is going to be replaced.
@@ -69,7 +68,7 @@
     /**
      * Maintaines the array of the snake. Adds at the beginning of the array the current position and 
      * removes from the end of the array the position which is not needed any more.
-     * @param {*} coordinates Gets an object which contains coordinates (number of row and column)
+     * @param {Object} coordinates Gets an object which contains coordinates (number of row and column)
      * @param {string} action In argument "action", expected value is string "grow" if the snake
      * is about to grow. In every other case, it is omitted. It's like overloading
      * the function.
@@ -78,9 +77,6 @@
         if (action === "grow") {
             snake.unshift(coordinates);
             changeBlockState(snake[0], "empty", "green")
-            // clearInterval(exec);
-            // speed();
-            // execute();
         } else {
             if (snake.length === 1) {
                 snake.unshift(coordinates);
@@ -133,7 +129,7 @@
 
     /**
      * Checks if the active block is already occupied by the snake (has the "green" class).
-     * @param block An object with coordinates.
+     * @param {Object} block An object with coordinates.
      */
     function isOccupiedBySnake(block) {
         if (document.getElementById(block.row + "-" + block.column).classList.contains("green")) {
@@ -146,7 +142,10 @@
      */
     function checkGameOver() {
         // Snake goes beyond board.
-        if (activeBlock.row > (dimensions.rows - 1) || activeBlock.row < 0 || activeBlock.column > (dimensions.columns - 1) || activeBlock.column < 0) {
+        if (activeBlock.row > (dimensions.rows - 1)
+            || activeBlock.row < 0
+            || activeBlock.column > (dimensions.columns - 1)
+            || activeBlock.column < 0) {
             stopExecution();
             alert("Game Over");
         }
@@ -158,7 +157,9 @@
         }
     }
 
-
+    /**
+     * Determines the next position of the apple.
+     */
     function determineNewApplePosition() {
         // if apple coordinates have been set, change current state from "apple" to "empty".
         if (apple.row != null || apple.column != null) {
@@ -183,8 +184,8 @@
 
     /**
      * Creates the game board
-     * @param {int} rows The number of rows of the board
-     * @param {Integer} columns The number of columns of the board
+     * @param {!number} rows The number of rows of the board
+     * @param {!number} columns The number of columns of the board
      */
     function createBoard(rows, columns) {
         document.body.innerHTML += "\n\n";
@@ -198,14 +199,14 @@
                 newElement = document.createElement("div");
 
                 // Applying the classes and alternating the colors
-                if (i % 2 == 0) {
-                    if (j % 2 == 0) {
+                if (i % 2 === 0) {
+                    if (j % 2 === 0) {
                         newElement.setAttribute("class", "block empty bg-color1");
                     } else {
                         newElement.setAttribute("class", "block empty bg-color2");
                     }
                 } else {
-                    if (j % 2 == 0) {
+                    if (j % 2 === 0) {
                         newElement.setAttribute("class", "block empty bg-color2");
                     } else {
                         newElement.setAttribute("class", "block empty bg-color1");
@@ -219,50 +220,45 @@
                 document.getElementById("row" + i).innerHTML += "\n"
             }
 
-            
-
             document.body.innerHTML += "\n\n";
         }
 
-        console.log(document.body.innerHTML);
+        // console.log(document.body.innerHTML);
+
         for (let i = 0; i < dimensions.columns; i++) {
             document.getElementById("0-" + i).classList.add("border-top");
-            document.getElementById((dimensions.rows - 1) + "-" + i).classList.add("border-bottom");   
+            document.getElementById((dimensions.rows - 1) + "-" + i).classList.add("border-bottom");
         }
-        
+
         for (let i = 0; i < dimensions.rows; i++) {
-            document.getElementById(i + "-0").classList.add("border-left");   
-            document.getElementById(i + "-" + (dimensions.columns - 1)).classList.add("border-right");   
-
+            document.getElementById(i + "-0").classList.add("border-left");
+            document.getElementById(i + "-" + (dimensions.columns - 1)).classList.add("border-right");
         }
-        
-
-
     }
 
-
-    // determine which arrow key is pressed and store info in variable 'direction'
+    // Determines which arrow key is pressed and store info in variable 'direction'
     // prevents snake from going on the opposite direction, like passing over itself.
-    document.onkeydown = function (e) {
-        if (e.keyCode == 37) {
-            if (direction != "right") {
+    document.addEventListener("keydown", e => {
+        if (e.keyCode === 37) {
+            if (direction !== "right") {
                 direction = "left";
             }
-        } else if (e.keyCode == 38) {
-            if (direction != "down") {
+        } else if (e.keyCode === 38) {
+            if (direction !== "down") {
                 direction = "up";
             }
-        } else if (e.keyCode == 39) {
-            if (direction != "left") {
+        } else if (e.keyCode === 39) {
+            if (direction !== "left") {
                 direction = "right";
             }
-        } else if (e.keyCode == 40) {
-            if (direction != "up") {
+        } else if (e.keyCode === 40) {
+            if (direction !== "up") {
                 direction = "down";
             }
+        } else if (e.keyCode === 27) {
+            alert("Game paused.");
         }
-    }
-
+    })
 
 
 })();
